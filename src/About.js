02ac7417edc7c7f53c7cefa5c37bef828a16f74a
br8S <56180503+br8S@users.noreply.github.com>
2021-03-21 16:03:44 -0700

    added content to about page and gallery

--- src/About.js
+++ src/About.js
@@ -10,6 +10,22 @@ function About() {
                     <h1 className="section-title">Personalized and total service.</h1>
                 </div>
             </header>
+            <div className="homeContent">
+                <div className="section-container">
+                    <p className="home-motto"><span>Our Residential and Commercial Work Speaks for Itself.</span> There's nothing more important than your landscape, so we tackle every project like it was our own. This commitment to landscape excellence has resulted in a list of very satisfied clients. We'd love to add your name to it. A well manicured landscape is a source of pride for any home or business owner. At Cal Trends Landscape, we take pride in the quality and consistency of the maintenance services we provide. From a one-time chemical application, to a full-service year round maintenance contract, Cal Trends Landscape Maintenance has the staff and equipment that will make your property the most beautiful in your neighborhood.
+
+                    <br />
+                    <br />Lawn care
+                    <br />Tree Care
+                    <br />Weed Control
+                    <br />Insect Control
+                    <br />Irrigation Services
+                    <br />Tree & Shrub Pruning
+                    <br />
+
+                    <br />Cal Trends Landscaping provides landscape maintenance services to all San Diego County.</p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Gallery.js
+++ src/Gallery.js
@@ -1,6 +1,73 @@
 import React from 'react';
+import imgOne from './images/pic-1.jpg';
+import imgTwo from './images/pic-2.jpg';
+import imgThree from './images/pic-3.jpg';
+import imgFour from './images/pic-4.jpg';
+import imgFive from './images/pic-5.jpg';
+import imgSix from './images/pic-6.jpg';
 
 function Gallery() {
+    var slideIndex = 0;
+
+    const startSlide = () => {
+        var slides = document.querySelectorAll('.mySlides');
+        slides[0].style.display = "block"
+    }
+
+    const showSlide = (event) => {
+        var slides = document.querySelectorAll('.mySlides');
+        var dots = document.querySelectorAll('.dot');
+
+        for(var i = 0; i < slides.length; i++){
+            if(dots[i] !== event.currentTarget){
+                slides[i].style.display = "none"
+            }
+            else {
+                slides[i].style.display = "block"
+            }
+        }
+    
+    }   
+
+    const nextSlide = () => {
+        var slides = document.querySelectorAll('.mySlides');
+
+        if((slideIndex + 1) < slides.length){
+            slideIndex += 1;
+        }
+        else{
+            slideIndex = 0;
+        }
+    
+        slides[slideIndex].style.display = "block"
+
+        for(var i = 0; i < slides.length; i++){
+            if(i !== slideIndex){
+                slides[i].style.display = "none"
+            }
+        }
+    }   
+
+    const prevSlide = () => {
+        var slides = document.querySelectorAll('.mySlides');
+
+        if((slideIndex - 1) > 0){
+            slideIndex -= 1;
+        }
+
+        else{
+            slideIndex = 5;
+        }
+    
+        slides[slideIndex].style.display = "block"
+
+        for(var i = 0; i < slides.length; i++){
+            if(i !== slideIndex){
+                slides[i].style.display = "none"
+            }
+        }
+    }
+
     return(
         <div>
             <header>
@@ -9,6 +76,62 @@ function Gallery() {
                     <h1 className="section-title">Check out some of our work.</h1>
                 </div>
             </header>
+
+            <div className="slideshow">
+                    <div className="slideshow-container">
+
+                        <div className="mySlides fade">
+                            <div className="numbertext"> </div>
+                            <img className="gallery-image" src={ imgOne }  onLoad={ startSlide } alt=''></img>
+                            <div className="text"> </div>
+                        </div>
+        
+                        <div className="mySlides fade">
+                            <div className="numbertext"> </div>
+                            <img className="gallery-image" src={ imgTwo } alt=''></img>
+                            <div className="text"> </div>
+                        </div>
+        
+                        <div className="mySlides fade">
+                            <div className="numbertext"> </div>
+                            <img className="gallery-image" src={ imgThree } alt=''></img>
+                            <div className="text"> </div>
+                        </div>
+
+                        <div className="mySlides fade">
+                            <div className="numbertext"> </div>
+                            <img className="gallery-image" src={ imgFour } alt=''></img>
+                            <div className="text"> </div>
+                        </div>
+
+                        <div className="mySlides fade">
+                            <div className="numbertext"> </div>
+                            <img className="gallery-image" src={ imgFive } alt=''></img>
+                            <div className="text"> </div>
+                        </div>
+
+                        <div className="mySlides fade">
+                            <div className="numbertext"> </div>
+                            <img className="gallery-image" src={ imgSix } alt=''></img>
+                            <div className="text"> </div>
+                        </div>
+        
+                        <a className="prev" onClick={ prevSlide }>&#10094;</a>
+                        <a className="next" onClick={ nextSlide }>&#10095;</a>
+                    </div>
+        
+                    <div className="dot-container">
+                        <span className="dot" onClick={ showSlide }></span>
+                        <span className="dot" onClick={ showSlide }></span>
+                        <span className="dot" onClick={ showSlide }></span>
+                        <span className="dot" onClick={ showSlide }></span>
+                        <span className="dot" onClick={ showSlide }></span>
+                        <span className="dot" onClick={ showSlide }></span>
+                    </div>
+                </div>
+
+                <p className="home-motto"><span>Our work speaks for itself.</span> We've collaborated with an array of wonderful clients on the most rewarding projects.</p>
+                
         </div>
     )
 }
