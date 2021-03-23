faf12af094ede579fe5b5ce713108f91200cddaa
br8S <56180503+br8S@users.noreply.github.com>
2021-03-22 18:11:08 -0700

    made soem changes to mobile view

--- src/Commercial.js
+++ src/Commercial.js
@@ -9,7 +9,7 @@ function Commercial() {
             <header>
                 <div className="headerContent">
                     <h3 className="section-name">Commercial/Residential &#9473;&#9473;</h3>
-                    <h1 className="section-title">Make your space a personal.</h1>
+                    <h1 className="section-title">Make your space a personal one.</h1>
                 </div>
             </header>
 
